707a6e7a58f3852763adbda06c643f443ea6e03d
shalizi34 <shalizi.shaliz@gmail.com>
2020-05-20 07:40:56 -0400

    TestNG Intro

--- src/com/class01/TaskTwo.java
+++ src/com/class01/TaskTwo.java
@@ -1,41 +1,42 @@
 package com.class01;
 
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class TaskTwo {
-	
+
 	@BeforeClass
 	public void beforeClass() {
-		System.out.println("This is a before Class");
+		System.out.println("This is before Class");
 	}
-	
+
 	@AfterClass
-	public void beforeAfter() {
+	public void afterClass() {
 		System.out.println("This is after Class");
 	}
-	
+
 	@BeforeMethod
-	public void beforeMethod () {
-		System.out.println("This is before method");
+	public void beforeMethod() {
+		System.out.println("This is before Method");
 	}
-	
+
 	@AfterMethod
 	public void afterMethod() {
-		System.out.println("This is after method");
+		System.out.println("This is after Method");
 	}
 
 	@Test
 	public void test1() {
-		System.out.println("This is test 1");	
+		System.out.println("This is test 1");
 	}
-	
+
 	@Test
 	public void test2() {
 		System.out.println("This is test 2");
-		
 	}
+
+	@Test
+	public void test3() {
+		System.out.println("This is test 3");
+	}
+
 }
--- src/com/class01/TestNGDemo.java
+++ src/com/class01/TestNGDemo.java
@@ -4,20 +4,21 @@ import org.testng.annotations.Test;
 
 public class TestNGDemo {
 
-@Test
-public void testOne() {
-	System.out.println("STEP 1");
-	System.out.println("STEP 2");
-	System.out.println("STEP 3");
-	
+	@Test
+	public void testOne() {
+		System.out.println("Step 1");
+		System.out.println("Step 2");
+		System.out.println("Step 3");
 	}
-@Test
-public void testTwo() {
-	System.out.println("Test 2");
+
+	@Test
+	public void testTwo() {
+		System.out.println("Test Case 2");
+	}
+
+	@Test
+	public void testThree() {
+		System.out.println("Test Case 3");
+	}
+
 }
-@Test
-public void testThree() {
-	System.out.println("Test 3");
-}
-}
-	
