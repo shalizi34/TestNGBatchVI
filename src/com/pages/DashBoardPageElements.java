c9e166a2bd710a1d3e3a9b425e55d7c55e46ea48
shalizi34 <shalizi.shaliz@gmail.com>
2020-05-23 10:10:51 -0400

    Assertion

---
+++ src/com/pages/DashBoardPageElements.java
@@ -0,0 +1,20 @@
+package com.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.utils.BaseClass;
+
+public class DashBoardPageElements {
+
+	@FindBy(id = "welcome")
+	public WebElement welcome;
+	
+	@FindBy(xpath = "//div[@id='branding']/a[1]/img")
+	public WebElement logo;
+
+	public DashBoardPageElements() {
+		PageFactory.initElements(BaseClass.driver, this);
+	}
+}
